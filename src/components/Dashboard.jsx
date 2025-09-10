0c7482c302891bfb6d0ba6f59eca6f15f9553e0a
Figaarillo <axel.leonardi.22@gmail.com>
2025-09-10 18:38:56 -0300

    feat: desarrollo del componente Navbar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,12 +2,15 @@ import { Lock } from "lucide-react"
 import { Link } from "react-router-dom"
 
 const Dashboard = ({isAdmin = true}) => {
-  return isAdmin && (
-    <Link className="bg-emerald-700 hover:bg-emerald-600 text-white py-2 px-4 rounded-md flex items-center transition duration-300 ease-in-out font-medium">
-      <Lock className="inline-block mr-1" size={18}/>
-      <span className="hiden sm:inline ml-2">Dashboard</span>
-    </Link>
-  )
+  return (
+    isAdmin && (
+      <Link
+        className="bg-emerald-700 hover:bg-emerald-600 text-white px-3 py-1 rounded-md font-medium transition duration-300 ease-in-out flex items-center" to={"/secret-dashboard"}>
+        <Lock className="inline-block mr-1" size={18} />
+        <span className="hiden sm:inline ml-2">Dashboard</span>
+      </Link>
+    )
+  );
 }
 
 export default Dashboard
