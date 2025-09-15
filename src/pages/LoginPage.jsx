97fef6604774f709e9fdff15d3c216dabfb39103
Figaarillo <axel.leonardi.22@gmail.com>
2025-09-15 19:06:00 -0300

    se agrego el bonton de submit al formulario

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -36,6 +36,10 @@ const LoginPage = () => {
               onChange={setPassword}
             />
 
+            <button
+             className="w-full flex justify-center border rounded-md border-transparent bg-emerald-600 py-2 px-4 text-sm font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2"
+             type="submit"
+             >Enviar  </button>
           </form>
         </div>
       </div>
