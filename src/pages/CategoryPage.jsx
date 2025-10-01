1e17c11a930a10aabd01e9375784b8d8f45addcb
Figaarillo <axel.leonardi.22@gmail.com>
2025-10-01 19:47:53 -0300

    feat: creo el componente product card

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ProductCard from "../components/ProductCard";
 
 const CategoryPage = () => {
   const { category } = useParams();
@@ -7,24 +8,26 @@ const CategoryPage = () => {
 
   useEffect(() => {
     fetchProductsByCategory();
-  })
+  });
+  //mens-clothing
+  //https://fakestoreapi.com/products/category/mens-clothing
 
   async function fetchProductsByCategory() {
     try {
-      const response = await fetch(`https://fakestoreapi.com/products/category/${category}`);
+      const response = await fetch(
+        `https://fakestoreapi.com/products/category/${category}`
+      );
       const data = await response.json();
       setProducts(data);
     } catch (error) {
       console.error('Error fetching products:', error);
     }
-  } 
+  }
 
   return (
     <div className="min-h-screen">
       <div className="relative z-10 max-w-screen-xl mx-auto px-4 sm:px--6 lg:px-8 py-16">
-        <h1
-        className="text-4xl font-extrabold text-emerald-400 sm:text-5xl md:text-6xl lg:text-7xl mb-8"
-        >
+        <h1 className="text-4xl font-extrabold text-emerald-400 sm:text-5xl md:text-6xl lg:text-7xl mb-8">
           {category.charAt(0).toUpperCase() + category.slice(1)}
         </h1>
 
@@ -35,13 +38,13 @@ const CategoryPage = () => {
             </h2>
           )}
 
-          {products.map((product) => (
+          {products.map(product => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default CategoryPage
